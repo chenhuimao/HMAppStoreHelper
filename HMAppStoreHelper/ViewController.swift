702666ba027d52ff53c3771ba62cccbb8fcf314d
陈晖茂 <chenhuimao@yeah.net>
2019-05-14 15:29:31 +0800

    Initial Commit

---
+++ HMAppStoreHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HMAppStoreHelper
+//
+//  Created by CHM on 2019/5/14.
+//  Copyright © 2019 pal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
